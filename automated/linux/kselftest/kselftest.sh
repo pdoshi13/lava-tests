1b3c9d24458c6dfdd39c0bb3d2e5993ba9e3074d
pdoshi13 <priyen.doshi@windriver.com>
2025-02-26 11:24:58 -0800

    Update kselftest.sh
    kselftest for x86_64

--- automated/linux/kselftest/kselftest.sh
+++ automated/linux/kselftest/kselftest.sh
@@ -27,6 +27,8 @@ SHARD_INDEX=1
 # Architecture-specific tarball name defaults.
 if [ "$(uname -m)" = "aarch64" ]; then
     TESTPROG="kselftest_aarch64.tar.gz"
+elif  [ "$(uname -m)" = "x86_64" ]; then 
+    TESTPROG="kselftests_x86-64_v6.1.123.tar.gz"
 else
     TESTPROG="kselftest_armhf.tar.gz"
 fi
@@ -36,7 +38,7 @@ usage() {
                     [-n number of shard buckets to create ]
                     [-c bpf cpufreq net timers]
                     [-T cpu-hotplug:cpu-on-off-test.sh]
-                    [-t kselftest_aarch64.tar.gz | kselftest_armhf.tar.gz]
+                    [-t kselftest_aarch64.tar.gz | kselftest_armhf.tar.gz|kselftests_x86-64_v6.1.123.tar.gz]
                     [-s True|False]
                     [-u url]
                     [-p path]
